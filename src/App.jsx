a9a5eb2c193cd5c0b0f4e1c3515851aa8be41eb7
Toni M <tonims.dev@gmail.com>
2022-02-02 09:48:17 +0100

    fix style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import { useState } from "react";
-import Axios from "axios";
+import axios from "axios";
 const App = () => {
   const [pokemonName, setPokemonName] = useState("");
 
@@ -18,12 +18,11 @@ const App = () => {
   });
 
   const searchPokemon = () => {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
       (res) => {
         setPokemon({
           name: pokemonName,
           number: res.data.id,
-          species: res.data.species.name,
           image: res.data.sprites.other.home.front_default,
           hp: res.data.stats[0].base_stat,
           attack: res.data.stats[1].base_stat,
@@ -41,13 +40,12 @@ const App = () => {
         <h1>Pokédex</h1>
         <input
           type="text"
-          placeholder="Pokémon"
+          placeholder="Write a Pokémon"
           onChange={(event) => {
             setPokemonName(event.target.value);
           }}
           value={pokemonName.toLowerCase()}
         />
-        
           {pokemonName && (
             <button onClick={searchPokemon}>Search Pokémon</button>
           )}
@@ -64,7 +62,6 @@ const App = () => {
             <div className="card">
             <img src={pokemon.image} alt={pokemon.name} />
             </div>
-            {/* <h3>{pokemon.species}</h3> */}
             <h3>Type: {pokemon.type}</h3>
             <h4>Hp: {pokemon.hp}</h4>
             <h4>Attack: {pokemon.attack}</h4>
